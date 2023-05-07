d25e4202f6e9493e9a2822dd1e03211f9a3dcec2
Kittiphon Julapak <111670386+KittiDJu@users.noreply.github.com>
2023-05-07 16:00:03 +0700

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host : 'aws.connect.psdb.cloud',
-    user : 'ayrmxj2assz6rp9jqpw9',
-    password : 'pscale_pw_cmhhqQaT5DV66QIEcBBnxVFxTJKPo2QFI9XZRmsg8sj',
+    host : 'localhost:8080',
+    user : '',
+    password : '',
     database : 'anywherekeystore'
 });
 
